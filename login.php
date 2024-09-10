947e29cfb3b46055c9fa52b05c26e0e29064698b
Averium <104313451+TrueAverium@users.noreply.github.com>
2024-09-10 19:35:57 -0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,44 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $firstname = $_POST["firstname"];
+
+    try{
+        require_once "dbh.inc.php";
+
+        $query = "SELECT EXISTS( SELECT 1 FROM users WHERE first_name = :firstname) AS name_exists";
+        $stmt = $pdo->prepare($query);
+
+        $stmt->execute([':firstname' => $firstname]);
+
+	$result = $stmt->fetch(PDO::FETCH_ASSOC);
+	
+	if ((bool)$result['name_exists']){
+		header("Location: ../contact.php");
+        	exit();
+	}
+	else {
+		$error = urlencode("Name not found");
+		header("Location: ../bad.php?error=" . $result['name_exists']);
+        	exit();
+	}
+
+
+        $pdo = null;
+        $stmt = null;
+
+        
+
+    }catch(PDOException $e)
+    {
+    die("Failed: ". $e->getMessage());
+    }
+
+}
+else{
+        header("Location: ../index.php");
+        exit();
+}
+
+?>
