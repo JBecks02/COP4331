64c8e980081391cafd1f205f3a831acc9aa65820
JBecks02 <jakecbecks02@gmail.com>
2024-09-10 10:59:54 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ID=edge">
+        <title>Your Page Title</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+           
+        </style>
+    </head>
+
+    <body>
+        <main>
+
+        <!-- Below is the log in portion of our home page -->
+        <h1>
+            <div class="wrapper">
+                <form action="">
+                    <h1>Login</h1>
+                    <div class="inpu">
+                        <input type="Name" placeholder="firstname" required>
+                    </div>
+                    <div>
+                        <input type="Lastname" placeholder="lastname" required>
+                    </div>
+                    <div class="remember-forget">
+                        <label><input type="checkbox"> Remember me</label>
+                    </div>
+
+                </form>
+            </div>
+        </h1>
+        <!-- End of the log in area of the home page -->
+
+
+        <div>
+            <div>
+                
+            </div>
+        </div>
+
+
+
+        <!-- Your content goes here -->
+        <form action="" method="post"></form>
+            <label for="firstname">Firstname</label>
+            <input id="firstname" type="text" name="firstname" placeholder="Firstname...">
+
+        <label for="firstname">Lastname</label>
+            <input id="lastname" type="text" name="lastname" placeholder="lastname">
+        <input id="" type="checkbox">
+
+
+        
+
+        <div>
+            <a href="https://www.tiktok.com/@classiedevva3/video/7396798316534631723" class="sleek-button">Spagetti</a>
+        </div>
+        
+
+    </body>
+
+
+##commentdssdfaslfjasAvery
+
+<html>
