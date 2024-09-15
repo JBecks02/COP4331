cc48d53e9edc7f133ffc547f85976e4b5368f58f
alanys2426 <al162520@ucf.edu>
2024-09-15 11:57:51 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,76 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ID=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Contact Manager</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ID=edge">
-        <link rel="stylesheet" type="text/css" href="style.css">
-        <title>Contact Manager</title>
-    </head>
+<body>
+    <main>
 
-    <body>
-        <main>
+        <!-- LOGIN -->
+        <div class="wrapper">
+            <form action="login.php" method="post">
+                <h1>Welcome back!</h1>
+                <div class="input-box">
+                    <input type="text" name="username" placeholder="Username" required>
+                </div>
+                <div class="input-box">
+                    <input type="text" name="password" placeholder="Password" required>
+                </div>
+                <div class="remember-forgot">
+                    <label><input type="checkbox"> Remember me</label>
+                </div>
 
-        <!-- Below is the log in portion of our home page -->
-            <div class="wrapper">
-                <form action="login.php" method="post">
-                    <h1>Login</h1>
-                    <div class="input-box">
-                        <input type="text" name="username" placeholder="username" required>
-                    </div>
-                    <div class="input-box">
-                        <input type="text" name="password" placeholder="password" required>
-                    </div>
-                    <div class="remember-forget">
-                    
-                    </div>
-                    <button class="btn">Login</button>
-                </form>
-            </div>
-        <!-- End of the log in area of the home page -->
-
-
-
-
-
-            <div class="wrapper">
-
-                <form action="formhandler.inc.php" method="post">
+                <button type="submit" class="btn">Login</button>
+            </form>
+        </div>
+        <div class = "table2">
+            <!-- NEW USER -->
+            <form action="formhandler.inc.php" method="post">
                 <h1>New User</h1>
-
-            
-            
-                
-
-                <div class="input-box">
-                <input  required id="firstname" type="text" name="firstname" placeholder="First Name" width="1004">
+                <div class="box">
+                    <input type="text" name="firstname" placeholder="First Name" required>
+                </div>
+                <div class="box">
+                    <input type="text" name="lastname" placeholder="Last Name" required>
+                </div>
+                <div class="box">
+                    <input type="text" name="username" placeholder="Username" required>
+                </div>
+                <div class="box">
+                    <input type="text" name="password" placeholder="Password" required>
                 </div>
 
-                
-
-                <div class="input-box">
-                <input required id="lastname" type="text" name="lastname" placeholder="Last Name">
-                </div>
-
-                
-
-                <div class="input-box">
-	            <input required id="username" type="text" name="username" placeholder="Username">
-                </div>
-
-                
-                <div class="input-box">
-	            <input required id="password" type="text" name="password" placeholder="Password">
-                </div>
-
-                <button class="btn" type="Submit">Submit</button>
-                </div>
-            </div>
-        </form>
-    
-
-
-    </body>
+                <button type="submit" class="btn">Sign up</button>
+            </form>
+        </div>
+</body>
 <html>
