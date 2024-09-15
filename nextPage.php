75793ea590a936ee947159bac2e6fedeab779573
alanys2426 <al162520@ucf.edu>
2024-09-15 14:33:31 -0400

    Update nextPage.php

--- nextPage.php
+++ nextPage.php
@@ -7,7 +7,7 @@ session_start();
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ID=edge">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Your Page Title</title>
         <link rel="stylesheet" type="text/css" href="style.css">
         <title>Contact Manager</title>
