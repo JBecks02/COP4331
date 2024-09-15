57af13a33b1beb6ad0ae9ff6165a3573092ff025
Averium <104313451+TrueAverium@users.noreply.github.com>
2024-09-15 12:35:35 -0400

    Add files via upload

---
+++ signUp.php
@@ -0,0 +1,42 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+	$firstname = $_POST["firstname"];
+	$lastname = $_POST["lastname"];
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+
+    try{
+        require_once "dbh.inc.php";
+
+        $query = "INSERT INTO users (first_name, last_name, username, password) VALUES (:firstname, :lastname, :username, :password);";
+        $stmt = $pdo->prepare($query);
+
+	$stmt->execute([
+		':firstname' => $firstname,
+		':lastname' => $lastname,
+		':username' => $username,
+		':password' => $password,
+	]);
+
+	$userID = $pdo->lastInsertId();
+
+	$_SESSION['userID'] = $userID;
+
+        $pdo = null;
+	$stmt = null;
+
+	header("Location: ../nextPage.php");
+        exit();
+
+    }catch(PDOException $e)
+    {
+    die("Failed: ". $e->getMessage());
+    }
+
+}
+else{
+	header("Location: ../index.php");
+	exit();
+}
